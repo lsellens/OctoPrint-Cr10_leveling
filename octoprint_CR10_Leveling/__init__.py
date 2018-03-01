a34e2bdb99fa15452ea12d750d29a432b2b0d0c3
electr0sheep <electr0sheep@electr0sheep.com>
2018-03-01 16:37:48 -0700

    Tried to get plugin in more friendly state
    I added a check to delete legacy code so people don't have to ssh into their devices.
    
    I put a warning message with the controls until I figure out a better solution. Incidently, OctoPrint's controls themselves have this exact same problem. So I guess that means I'm in uncharted waters with this.
    
    I added a feed rate setting so people can make the machine haul butt if they want to. I'm pretty sure most machines have a maximum that you can't exceed, but I look forward to having someone's bed go flying into the wall.

--- octoprint_CR10_Leveling/__init__.py
+++ octoprint_CR10_Leveling/__init__.py
@@ -1,19 +1,32 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+from octoprint.settings import settings
 import octoprint.plugin
 
 
 class Cr10_levelingPlugin(octoprint.plugin.AssetPlugin,
                           octoprint.plugin.TemplatePlugin,
-                          octoprint.plugin.SettingsPlugin):
+                          octoprint.plugin.SettingsPlugin,
+                          octoprint.plugin.StartupPlugin):
+
+    def on_after_startup(self):
+        s = settings()
+
+        controlList = s.get(["controls"])
+
+        for item in controlList:
+            if (item['name'] == 'CR-10 Bed Leveling'):
+                controlList.remove(item)
+
+        s.set(["controls"], controlList)
 
     def get_settings_defaults(self):
         return dict(bed_temp=50, nozzle_temp=170, play_tune=True,
                     front_left_x=30, front_left_y=30, front_right_x=270,
                     front_right_y=30, back_left_x=30, back_left_y=270,
                     back_right_x=270, back_right_y=270, center_x=150,
-                    center_y=150)
+                    center_y=150, feed_rate=3600)
 
     def get_template_configs(self):
         return [dict(type="settings", custom_bindings=False)]
--- octoprint_CR10_Leveling/static/cr10leveling.js
+++ octoprint_CR10_Leveling/static/cr10leveling.js
@@ -9,7 +9,9 @@ $(function() {
       if (settings.play_tune()) {
         return [{
           'customClass': '', 'layout': 'horizontal_grid', 'name': 'CR-10 Bed Leveling', 'children':
-            [{'width': '11', 'commands':
+            [{'width': '11', 'output': 'WARNING: DO NOT USE CONTROLS WITHOUT HOMING FIRST!!!'},
+            {'width': '11', 'output': 'If you changed settings, make sure you refresh the page'},
+            {'width': '11', 'commands':
               ['M140 S' + settings.bed_temp(), 'M104 S' + settings.nozzle_temp(),
               'M190 S' + settings.bed_temp(), 'M109 S' + settings.nozzle_temp(),
               'M300 S0 P66', 'M300 S987 P66', 'M300 S0 P66', 'M300 S1975 P66',
@@ -28,24 +30,24 @@ $(function() {
               'M300 S1975 P100'],
             'customClass': 'btn btn-danger', 'name': 'Apply Heat', 'offset': '2'},
             {'width': '2', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.front_left_x() + 'Y' +
-              settings.front_left_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.front_left_x() + 'Y' +
+              settings.front_left_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Front Left'},
             {'width': '2', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.front_right_x() + 'Y' +
-              settings.front_right_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.front_right_x() + 'Y' +
+              settings.front_right_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Front Right', 'offset': '2'},
             {'width': '8', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.center_x() + 'Y' +
-              settings.center_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.center_x() + 'Y' +
+              settings.center_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Center', 'offset': '2'},
             {'width': '2', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.back_left_x() + 'Y' +
-              settings.back_left_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.back_left_x() + 'Y' +
+              settings.back_left_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Back Left'},
             {'width': '2', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.back_right_x() + 'Y' +
-              settings.back_right_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.back_right_x() + 'Y' +
+              settings.back_right_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Back Right', 'offset': '2'}]
         }]
       } else {
@@ -56,24 +58,24 @@ $(function() {
               'M190 S' + settings.bed_temp(), 'M109 S' + settings.nozzle_temp()],
             'customClass': 'btn btn-danger', 'name': 'Apply Heat', 'offset': '2'},
             {'width': '2', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.front_left_x() + 'Y' +
-              settings.front_left_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.front_left_x() + 'Y' +
+              settings.front_left_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Front Left'},
             {'width': '2', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.front_right_x() + 'Y' +
-              settings.front_right_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.front_right_x() + 'Y' +
+              settings.front_right_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Front Right', 'offset': '2'},
             {'width': '8', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.center_x() + 'Y' +
-              settings.center_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.center_x() + 'Y' +
+              settings.center_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Center', 'offset': '2'},
             {'width': '2', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.back_left_x() + 'Y' +
-              settings.back_left_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.back_left_x() + 'Y' +
+              settings.back_left_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Back Left'},
             {'width': '2', 'commands':
-              ['G1 Z10 F500', 'G1 X' + settings.back_right_x() + 'Y' +
-              settings.back_right_y() + 'F3600', 'G1 Z0 F500'],
+              ['G90', 'G0 Z10 F500', 'G0 X' + settings.back_right_x() + 'Y' +
+              settings.back_right_y() + 'F' + settings.feed_rate(), 'G0 Z0 F500'],
             'customClass': 'btn', 'name': 'Back Right', 'offset': '2'}]
         }]
       }
