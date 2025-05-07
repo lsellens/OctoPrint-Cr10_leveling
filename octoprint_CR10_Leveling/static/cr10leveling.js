425f51d4282e41a81c1eab087a373dc41d1c295d
lsellens <lsellens@gmail.com>
2025-05-07 09:46:12 -0500

    refactor code to reference buttons by name instead of index#

--- octoprint_CR10_Leveling/static/cr10leveling.js
+++ octoprint_CR10_Leveling/static/cr10leveling.js
@@ -16,120 +16,156 @@
 //
 // Email: electr0sheep@electr0sheep.com
 
-$(function() {
-  function MyCustomViewModel (parameters) {
-    var self = this;
-
+$(function () {
+  function MyCustomViewModel(parameters) {
+    const self = this;
     self.settings = parameters[0];
 
-    self.getAdditionalControls = function() {
-      var settings = self.settings.settings.settings.plugins.CR10_Leveling
+    function createButton({ width = '2', offset = '0', name, commands = [], command = null, output = null, customClass = 'btn', additionalClasses = 'nowrap' }) {
+      const button = {
+        width,
+        offset,
+        customClass,
+        additionalClasses,
+        name
+      };
+      if (output) {
+        button.output = output;
+      } else if (command) {
+        button.command = command;
+      } else {
+        button.commands = commands;
+      }
+      return button;
+    }
 
-      var baseSettings = [{
-        'customClass': '', 'layout': 'horizontal_grid', 'name': 'Bed Leveling', 'children':
-          [{'width': '2', 'commands':
-            [],
-          'customClass': 'btn', 'additionalClasses': 'btn-danger nowrap', 'name': 'Apply Heat'},
-          {'width': '7', 'commands':
-            ['M140 S0', 'M104 S0'],
-          'customClass': 'btn', 'additionalClasses': 'btn-warning nowrap', 'name': 'Stop Heat', 'offset': '2'},
-          {'width': '2', 'commands':
-            ['G90', 'G0 Z' + settings.upper_z() + ' F500', 'G0 X' +
-            settings.back_left_x() + ' Y' + settings.back_left_y() + ' F' +
-            settings.feed_rate(), 'G0 Z' + settings.lower_z() + ' F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Back \xa0Left\xa0'},
-          {'width': '7', 'commands':
-            ['G90', 'G0 Z' + settings.upper_z() + ' F500', 'G0 X' +
-            settings.back_right_x() + ' Y' + settings.back_right_y() + ' F' +
-            settings.feed_rate(), 'G0 Z' + settings.lower_z() + ' F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Back Right', 'offset': '2'},
-          {'width': '9', 'commands':
-            ['G90', 'G0 Z' + settings.upper_z() + ' F500', 'G0 X' +
-            settings.center_x() + ' Y' + settings.center_y() + ' F' +
-            settings.feed_rate(), 'G0 Z' + settings.lower_z() + ' F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Center', 'offset': '2'},
-          {'width': '2', 'commands':
-            ['G90', 'G0 Z' + settings.upper_z() + ' F500', 'G0 X' +
-            settings.front_left_x() + ' Y' + settings.front_left_y() + ' F' +
-            settings.feed_rate(), 'G0 Z' + settings.lower_z() + ' F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Front \xa0Left\xa0'},
-          {'width': '7', 'commands':
-            ['G90', 'G0 Z' + settings.upper_z() + ' F500', 'G0 X' +
-            settings.front_right_x() + ' Y' + settings.front_right_y() + ' F' +
-            settings.feed_rate(), 'G0 Z' + settings.lower_z() + ' F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Front Right', 'offset': '2'}]
-      }]
+    function applyHeatCommands(button, bed, nozzle) {
+      if (button && Array.isArray(button.commands)) {
+        button.commands.unshift(`M140 S${bed}`, `M104 S${nozzle}`);
+      }
+    }
 
+    function waitForHeatCommands(button, bed, nozzle) {
+      if (button && Array.isArray(button.commands)) {
+        button.commands.push(`M190 S${bed}`, `M109 S${nozzle}`);
+      }
+    }
+
+    function addTuneCommands(button) {
+      if (!button || !Array.isArray(button.commands)) return;
+      const tune = [
+        'M300 S0 P66', 'M300 S987 P66', 'M300 S0 P66', 'M300 S1975 P66',
+        'M300 S0 P66', 'M300 S2959 P66', 'M300 S0 P66', 'M300 S2489 P66',
+        'M300 S0 P66', 'M300 S1975 P66', 'M300 S2959 P66', 'M300 S0 P133',
+        'M300 S2489 P133', 'M300 S0 P133', 'M300 S2093 P66', 'M300 S0 P66',
+        'M300 S4186 P66', 'M300 S0 P66', 'M300 S3135 P66', 'M300 S0 P66',
+        'M300 S2637 P66', 'M300 S0 P66', 'M300 S4186 P66', 'M300 S3135 P66',
+        'M300 S0 P133', 'M300 S2637 P133', 'M300 S0 P133', 'M300 S987 P66',
+        'M300 S0 P66', 'M300 S1975 P66', 'M300 S0 P66', 'M300 S2959 P66',
+        'M300 S0 P66', 'M300 S2489 P66', 'M300 S0 P66', 'M300 S1975 P66',
+        'M300 S2959 P66', 'M300 S0 P133', 'M300 S2489 P133', 'M300 S0 P133',
+        'M300 S2489 P66', 'M300 S2637 P66', 'M300 S2793 P66', 'M300 S0 P66',
+        'M300 S2793 P66', 'M300 S2959 P66', 'M300 S3135 P66', 'M300 S0 P66',
+        'M300 S3135 P66', 'M300 S3322 P66', 'M300 S1760 P66', 'M300 S0 P66',
+        'M300 S1975 P100'
+      ];
+      button.commands.push(...tune);
+    }
+
+    self.getAdditionalControls = function () {
+      const settings = self.settings.settings.settings.plugins.CR10_Leveling;
+      const children = [];
+
+      children.push(createButton({ width: '11', output: 'If you changed settings, make sure you refresh the page' }));
+
+      // Optional warning banners
       if (settings.show_warning_banner()) {
-        baseSettings[0].children.splice(0, 0,
-          {'width': '11', 'output': 'If you changed settings, make sure you refresh the page'},
-          {'width': '11', 'output': 'WARNING: DO NOT USE CONTROLS WITHOUT HOMING FIRST!!!'}
-        );
+        children.push(createButton({ width: '11', output: 'WARNING: DO NOT USE CONTROLS WITHOUT HOMING FIRST!!!' }));
       }
 
+      // Apply/Stop heat buttons
+      const applyHeatButton = createButton({ name: 'Apply Heat', additionalClasses: 'btn-danger nowrap' });
+      const stopHeatButton = createButton({
+        name: 'Stop Heat',
+        commands: ['M140 S0', 'M104 S0'],
+        width: '7',
+        offset: '2',
+        additionalClasses: 'btn-warning nowrap'
+      });
+
+      children.push(applyHeatButton, stopHeatButton);
+
+      // Optional autolevel button
       if (settings.autolevel()) {
-        baseSettings[0].children[3].width = '2';
-        baseSettings[0].children[3].offset = '0';
-        var autolevel = settings.autolevel();
-        if (autolevel.indexOf('\n') == -1) {
-          baseSettings[0].children.splice(4, 0, {'width': '7', 'command': autolevel,
-          'customClass': 'btn', 'additionalClasses': 'btn-success', 'name': 'Autolevel'});
-        } else {
-          baseSettings[0].children.splice(4, 0, {'width': '7', 'commands': autolevel.split('\n'),
-          'customClass': 'btn', 'additionalClasses': 'btn-success', 'name': 'Autolevel'});
+        const stopIndex = children.findIndex(btn => btn.name === 'Stop Heat');
+        if (stopIndex !== -1) {
+          children[stopIndex].width = '2';
+          children[stopIndex].offset = '0';
         }
+
+        const autolevel = settings.autolevel();
+        const autolevelButton = createButton({
+          name: 'Autolevel',
+          width: '7',
+          offset: '0',
+          additionalClasses: 'btn-success',
+          ...(autolevel.includes('\n')
+            ? { commands: autolevel.split('\n') }
+            : { command: autolevel })
+        });
+
+        children.splice(stopIndex + 1, 0, autolevelButton);
       }
 
-      var finalSettings = baseSettings;
+      // Leveling position buttons
+      const positions = [
+        { name: 'Back \xa0Left\xa0', x: settings.back_left_x(), y: settings.back_left_y(), offset: '0' },
+        { name: 'Back Right', x: settings.back_right_x(), y: settings.back_right_y(), offset: '2' },
+        { name: 'Center', x: settings.center_x(), y: settings.center_y(), offset: '2', width: '9' },
+        { name: 'Front \xa0Left\xa0', x: settings.front_left_x(), y: settings.front_left_y(), offset: '0' },
+        { name: 'Front Right', x: settings.front_right_x(), y: settings.front_right_y(), offset: '2' }
+      ];
 
+      positions.forEach(pos => {
+        children.push(createButton({
+          name: pos.name,
+          width: pos.width || '2',
+          offset: pos.offset,
+          commands: [
+            'G90',
+            `G0 Z${settings.upper_z()} F500`,
+            `G0 X${pos.x} Y${pos.y} F${settings.feed_rate()}`,
+            `G0 Z${settings.lower_z()} F500`
+          ]
+        }));
+      });
+
+      // Add heat commands
       if (settings.wait_for_heat()) {
-        waitForHeat(finalSettings);
+        waitForHeatCommands(applyHeatButton, settings.bed_temp(), settings.nozzle_temp());
+
         if (settings.play_tune()) {
-          applyTune(finalSettings);
+          addTuneCommands(applyHeatButton);
         }
       } else {
-          applyHeat(finalSettings);
+        applyHeatCommands(applyHeatButton, settings.bed_temp(), settings.nozzle_temp());
       }
-      return finalSettings;
+
+      if (settings.play_tune()) {
+        addTuneCommands(applyHeatButton);
+      }
+
+      return [{
+        customClass: '',
+        layout: 'horizontal_grid',
+        name: 'Bed Leveling',
+        children: children
+      }];
     }
-
-    function applyTune(currentSettings) {
-      var heatCommand = currentSettings[0].children[2].commands;
-
-      heatCommand.push('M300 S0 P66', 'M300 S987 P66', 'M300 S0 P66', 'M300 S1975 P66',
-      'M300 S0 P66', 'M300 S2959 P66', 'M300 S0 P66', 'M300 S2489 P66',
-      'M300 S0 P66', 'M300 S1975 P66', 'M300 S2959 P66', 'M300 S0 P133',
-      'M300 S2489 P133', 'M300 S0 P133', 'M300 S2093 P66', 'M300 S0 P66',
-      'M300 S4186 P66', 'M300 S0 P66', 'M300 S3135 P66', 'M300 S0 P66',
-      'M300 S2637 P66', 'M300 S0 P66', 'M300 S4186 P66', 'M300 S3135 P66',
-      'M300 S0 P133', 'M300 S2637 P133', 'M300 S0 P133', 'M300 S987 P66',
-      'M300 S0 P66', 'M300 S1975 P66', 'M300 S0 P66', 'M300 S2959 P66',
-      'M300 S0 P66', 'M300 S2489 P66', 'M300 S0 P66', 'M300 S1975 P66',
-      'M300 S2959 P66', 'M300 S0 P133', 'M300 S2489 P133', 'M300 S0 P133',
-      'M300 S2489 P66', 'M300 S2637 P66', 'M300 S2793 P66', 'M300 S0 P66',
-      'M300 S2793 P66', 'M300 S2959 P66', 'M300 S3135 P66', 'M300 S0 P66',
-      'M300 S3135 P66', 'M300 S3322 P66', 'M300 S1760 P66', 'M300 S0 P66',
-      'M300 S1975 P100')
-    }
-
-    function waitForHeat(currentSettings) {
-      var settings = self.settings.settings.settings.plugins.CR10_Leveling;
-      var heatCommand = currentSettings[0].children[2].commands;
-
-      heatCommand.push('M190 S' + settings.bed_temp(), 'M109 S' + settings.nozzle_temp());
-    }
-
-    function applyHeat(currentSettings) {
-      var settings = self.settings.settings.settings.plugins.CR10_Leveling;
-      var heatCommand = currentSettings[0].children[2].commands;
-
-      heatCommand.unshift('M140 S' + settings.bed_temp(), 'M104 S' + settings.nozzle_temp());
-    }
-
   }
 
   OCTOPRINT_VIEWMODELS.push({
     construct: MyCustomViewModel,
     dependencies: ["controlViewModel"]
   });
-})
+});
