bfbaebe8a8dff16371aed3d654a0c6c867f07184
lsellens <lsellens@gmail.com>
2025-08-15 18:42:19 -0500

    update project files

--- octoprint_manual_bed_tramming/__init__.py
+++ octoprint_manual_bed_tramming/__init__.py
@@ -4,23 +4,6 @@ It allows users to set various parameters such as nozzle temperature, bed temper
 and coordinates for tramming points.
 """
 
-# Copyright (C) 2018  electr0sheep
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Email: electr0sheep@electr0sheep.com
-
 from __future__ import absolute_import
 from typing import Any, Dict, Optional, Literal
 
@@ -62,40 +45,30 @@ class ManualBedTrammingPlugin(octoprint.plugin.AssetPlugin, # pylint: disable=to
                     lower_z=0, upper_z=10, feed_rate=3600, autolevel="",
                     has_heated_bed=heated_bed, has_heated_chamber=heated_chamber)
 
-    def get_template_configs(self):
+    def get_template_configs(self) -> dict:
         return [dict(type="settings", custom_bindings=False)]
 
     def get_assets(self) -> dict:
         """Return the assets (JavaScript, CSS) for the plugin."""
-        return dict(js=["js/manual_bed_tramming.js"], css=["css/manual_bed_tramming.css"])
+        return dict(js=["js/CR10_Leveling.js"], css=["css/CR10_Leveling.css"])
 
     def get_update_information(self) -> dict:
         """Return information about the plugin for software update checks."""
-        return dict(
-            CR10_Leveling=dict(
-                displayName="Manual Bed Tramming",
-                displayVersion=self._plugin_version,
+        return {
+            "CR10_Leveling": {
+                "displayName": "Manual Bed Tramming",
+                "displayVersion": self._plugin_version,
 
                 # version check: github repository
-                type="github_release",
-                user="lsellens",
-                repo="octoprint-manual_bed_tramming",
-                current=self._plugin_version,
-                stable_branch=dict(
-                    name="Stable", branch="master", comittish=["master"]
-                ),
-                prerelease_branch=[
-                    dict(
-                        name="Testing",
-                        branch="develop",
-                        comittish=["develop", "master"]
-                    )
-                ],
+                "type": "github_release",
+                "user": "lsellens",
+                "repo": "octoprint-manual_bed_tramming",
+                "current": self._plugin_version,
 
                 # update method: pip
-                pip="https://github.com/lsellens/octoprint-manual_bed_tramming/archive/{target_version}.zip"
-            )
-        )
+                "pip": "https://github.com/lsellens/octoprint-manual_bed_tramming/archive/{target_version}.zip"
+            }
+        }
 
     def is_template_autoescaped(self) -> Literal[True]:
         """Return whether the template should be auto-escaped."""
--- octoprint_manual_bed_tramming/static/js/CR10_Leveling.js
+++ octoprint_manual_bed_tramming/static/js/CR10_Leveling.js
@@ -1,23 +1,11 @@
-// OctoPrint Manual Bed Tramming Plugin. Allows user to easily tram 3D printer.
-// Copyright (C) 2018  electr0sheep
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// Email: electr0sheep@electr0sheep.com
-
+/*
+ * View model for octoprint-manual_bed_tramming
+ *
+ * Author: lsellens
+ * License: AGPL-3.0
+ */
 $(function () {
-  function MyCustomViewModel(parameters) {
+  function ManualBedTrammingViewModel(parameters) {
     const self = this;
     self.settings = parameters[0];
 
@@ -190,7 +178,7 @@ $(function () {
   }
 
   OCTOPRINT_VIEWMODELS.push({
-    construct: MyCustomViewModel,
+    construct: ManualBedTrammingViewModel,
     dependencies: ["settingsViewModel", "controlViewModel"]
   });
 });
